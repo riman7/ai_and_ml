34c7a1620f458a55267ff997b76bd9b95159c700
riman7 <r.i.m.a.n.maharjan7@gmail.com>
2025-05-14 23:57:02 +0545

    added

--- maze_A_star.py
+++ maze_A_star.py
@@ -1,7 +1,45 @@
 import sys
+import heapq
 
+import itertools
+
+
+# expand to node with lowest g(n) + h(n)
+# g(n) = cost to reach node
+# h(n) = estimated cost to reach goal
+
+class Node():
+    def __init__(self, state, parent, action, g, h):
+        self.state = state
+        self.parent = parent
+        self.action = action
+        self.g = g
+        self.h = h
+        self.f = g + h
+
+class PriorityQueueFrontier:
+    def __init__(self):
+        self.frontier = []
+        self.counter = itertools.count()  # tie-breaker
+    
+    def add(self, node):
+        count = next(self.counter)
+        heapq.heappush(self.frontier, (node.f, count, node))
+    
+    def contains_state(self, state):
+        return any(node.state == state for _, _, node in self.frontier)
+
+    
+    def empty(self):
+        return len(self.frontier) == 0
+    
+    def remove(self):
+        if self.empty():
+            raise Exception("Empty Frontier")
+        
+        return heapq.heappop(self.frontier)[2]
+    
 class Maze():
-
     def __init__(self, filename):
         # Read file and set height and width of maze
         with open(filename) as f:
@@ -57,3 +95,66 @@ class Maze():
                     print(" ", end="")
             print()
         print()
+
+    def neighbors(self, state):
+        row, col = state
+        candidates = [
+            ("up", (row - 1, col)),
+            ("down", (row + 1, col)),
+            ("left", (row, col - 1)),
+            ("right", (row, col + 1))
+        ]
+
+        result = []
+        for action, (r, c) in candidates:
+            if 0 <= r < self.height and 0 <= c < self.width and not self.walls[r][c]:
+                result.append((action, (r, c)))
+        return result
+    
+    def heuristic(self, a, b):
+        return abs(self.goal[0] - a) + abs(self.goal[1] - b)
+
+    def solve(self):
+        self.num_explored = 0
+        self.explored = set()
+
+        start = Node(state= self.start, parent=None, action= None, g = 0, h = self.heuristic(self.start[0], self.start[1]))
+
+        frontier = PriorityQueueFrontier()
+        frontier.add(start)
+
+        while(True):
+            if frontier.empty():
+                raise Exception("No solution")
+            
+            node = frontier.remove()
+            self.num_explored += 1
+
+            if node.state == self.goal:
+                cells = []
+                actions = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.solution = (actions, cells)
+                return
+
+            for action, state in self.neighbors(node.state):
+                if not frontier.contains_state(state) and state not in self.explored:
+                    child = Node(state=state, parent=node, action=action, g = node.g + 1, h = self.heuristic(self.start[0], self.start[1]))
+                    frontier.add(child)
+            
+
+    
+    
+if len(sys.argv) != 2:
+    sys.exit("Usage: python maze.py maze.txt")
+
+m = Maze(sys.argv[1])
+m.print()
+m.solve()
+print("States Explored:", m.num_explored)
+m.print()
